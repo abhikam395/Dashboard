acd6efb6404863bea1fa80ec5c2186aa61b18faf
abhikam395 <--add>
2021-04-20 08:28:51 +0530

    line graph created

--- src/components/LineGraphComponent.js
+++ src/components/LineGraphComponent.js
@@ -2,74 +2,90 @@ import React, {Component} from 'react';
 import './linegraph.scss';
 
 import {RiArrowDownSFill, RiArrowUpSFill} from 'react-icons/ri';
+import Chart from 'chart.js';
 
 export default class LineGraphComponent extends Component{
 
     componentDidMount(){
         let {data } = this.props;
-        this.drawLine(data.id - 1)
+        this.drawLine(data.id - 1);
     }
 
-    drawLine(id){   
+    drawLine(id){  
+        let context = null; 
         let {data} = this.props;
         var canvas = document.getElementsByClassName( "data__canvas")[id];  
-        var context = canvas.getContext( "2d" );
-        
-        // declare graph start and end  
-        var GRAPH_TOP = 100;  
-        var GRAPH_LEFT = 0;  
-        var GRAPH_RIGHT = 630;   
-        
-        var GRAPH_HEIGHT = 350;
-
-        // declare test data set and get length   
-        var dataArr = [ 6, 8, 10, 12, 11, 7, 5, 8 ];  
-        var arrayLen = dataArr.length;
-        // calculate largest piece of data  
-        
-        var largest = 0;  
-        for( var i = 0; i < arrayLen; i++ ){  
-            if( dataArr[ i ] > largest ){  
-                largest = dataArr[ i ];  
-            }  
-        }  
-
-        context.beginPath();   
-        // make your graph look less jagged  
-        context.lineJoin = "round";  
-        context.strokeStyle = data.percentage > 0 ? "#1987FF" : 'red'   
-        context.lineWidth = 6;
-        context.fillStyle = "#F2FBFD";
-        // add first point in the graph  
-        context.moveTo(GRAPH_LEFT, 460 );   
-        // loop over data and add points starting from the 2nd index in the array as the first has been added already  
-        for( var i = 1; i < arrayLen; i++ ){  
-            context.lineTo( GRAPH_RIGHT / arrayLen * i + GRAPH_LEFT, ( GRAPH_HEIGHT - dataArr[ i ] / largest * GRAPH_HEIGHT ) + GRAPH_TOP );  
+        if(canvas !== null){
+            context = canvas.getContext("2d");
+            var myChart = new Chart(context, {
+                type: 'line',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Orange'],
+                    datasets: [{
+                        label: 'My First Dataset',
+                        data: data.data,
+                        backgroundColor: data.color,
+                        borderColor: 'rgb(75, 192, 192)',
+                        borderWidth: 1,
+                      }]
+                },
+                options: {
+                    elements: {
+                        point:{
+                            radius: 0
+                        }
+                    },
+                    legend: {
+                        display: false,
+                    },
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                display: false,
+                                drawBorder: false,
+                                tickMarkLength: false,
+                            },
+                            ticks: {
+                                display: false
+                            }
+                        }],
+                        xAxes: [{
+                            gridLines: {
+                                display: false,
+                                drawBorder: false,
+                                tickMarkLength: false,
+                            },
+                            ticks: {
+                                display: false
+                            },
+                        }]
+                    }
+                }
+            })
         }
-        // context.fill();
-        // actually draw the graph  
-        context.stroke();
     }
 
     render(){
         let {data} = this.props;
         return (
             <li className="data data--size data--theme" id="data">
-                <h4 className="data__name">{data.name}</h4>
-                <h1 className="data__count">{data.count}</h1>
-                {data.percentage >= 0 && 
-                    <div className="data__row data__up">
-                        <RiArrowUpSFill className="data__icon"/>
-                        <span className="data__percentage">{data.percentage}</span>
-                    </div>
-                }
-                {data.percentage < 0 && 
-                    <div className="data__row data__down">
-                        <RiArrowDownSFill className="data__icon"/>
-                        <span className="data__percentage">{data.percentage}</span>
-                    </div>
-                }
-                <canvas className="data__canvas" id="canvas" width="550" height="450"></canvas>
+                <canvas className="data__canvas" />
+                <div className="data__info">
+                    <h4 className="data__name">{data.name}</h4>
+                    <h1 className="data__count">{data.count}</h1>
+                    {data.percentage >= 0 && 
+                        <div className="data__row data__up">
+                            <RiArrowUpSFill className="data__icon"/>
+                            <span className="data__percentage">{data.percentage}</span>
+                        </div>
+                    }
+                    {data.percentage < 0 && 
+                        <div className="data__row data__down">
+                            <RiArrowDownSFill className="data__icon"/>
+                            <span className="data__percentage">{data.percentage}</span>
+                        </div>
+                    }
+                </div>
         </li>
         )
     }
--- src/pages/BlogDashboardPage.js
+++ src/pages/BlogDashboardPage.js
@@ -14,31 +14,41 @@ export default class BlogDashboardPage extends Component{
                 id: 1,
                 name: 'POSTS',
                 count: '2,390',
-                percentage: 4.6
+                data: [0, 40, 30, 31, 50, 35, 10],
+                percentage: 4.6,
+                color: '#F1FAFC',
             },
             {
                 id: 2,
                 name: 'PAGES',
                 count: '390',
-                percentage: 1.3
+                data: [20, 10, 30, 41, 50, 45, 40],
+                percentage: 1.3,
+                color: '#F1FAFC',
             },
             {
                 id: 3,
                 name: 'COMMENTS',
                 count: '8,390',
-                percentage: -2.3
+                data: [10, 40, 40, 41, 50, 15, 10],
+                percentage: -2.3,
+                color: '#FFF5F7'
             },
             {
                 id: 4,
                 name: 'USERS',
                 count: '2,190',
-                percentage: 4.7
+                data: [10, 40, 30, 1, 50, 55, 40],
+                percentage: 4.7,
+                color: '#F1FAFC',
             },
             {
                 id: 5,
                 name: 'SUBSCRIBERS',
                 count: '12,190',
-                percentage: -4.7
+                data: [0, 40, 30, 31, 50, 32, 40],
+                percentage: -4.7,
+                color: '#FEFAF1',
             }
            ]
         }
@@ -46,7 +56,7 @@ export default class BlogDashboardPage extends Component{
 
     renderLineGraphs(list){
         return list.map(data => (
-            <LineGraphComponent data={data}/>
+            <LineGraphComponent data={data} key={data.id}/>
         ))
     }
 
